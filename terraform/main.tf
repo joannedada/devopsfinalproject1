39cb39adc573f6e9d3f74964746a2d20d74346d3
joannedada <joanneobodoagwu@gmail.com>
2025-05-25 21:44:32 +0100

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,6 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.k8s_vpc.id
   cidr_block              = "10.0.${count.index + 1}.0/24"
   availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
 }
 
 # S3 bucket for kOps state
